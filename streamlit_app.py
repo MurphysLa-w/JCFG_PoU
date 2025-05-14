ffb2167a128037456e92280581b26b81740cd8b0
Murphys Law <65669647+MurphysLa-w@users.noreply.github.com>
2025-05-14 19:54:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,17 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-if "scores" not in st.session_state:
-    st.session_state.scores = [
-        {"name": "Josh", "Pushups": 10, "Situps": 20},
-    ]
+df = pd.DataFrame(
+    [
+       {"command": "st.selectbox", "rating": 4, "is_widget": True},
+       {"command": "st.balloons", "rating": 5, "is_widget": False},
+       {"command": "st.time_input", "rating": 3, "is_widget": True},
+   ]
+)
+edited_df = st.data_editor(df, num_rows="dynamic")
 
-
-def new_scores():
-    st.session_state.scores.append(
-        {
-            "name": st.session_state.name,
-            "Pushups": st.session_state.pushups,
-            "Situps": st.session_state.situps,
-        }
-    )
-
-
-st.write("# Score table")
-
-score_df = pd.DataFrame(st.session_state.scores)
-score_df["total_points"] = score_df["Pushups"] + score_df["Situps"]
-
-st.write(score_df)
-
-st.write("# Add a new score")
-with st.form("new_score", clear_on_submit=True):
-    name = st.text_input("Name", key="name")
-    pushups = st.number_input("Pushups", key="pushups", step=1, value=0, min_value=0)
-    situps = st.number_input("Situps", key="situps", step=1, value=0, min_value=0)
-    st.form_submit_button("Submit", on_click=new_scores)
-
-
-if(st.button("Delete")):
-    #Delete all the items in Session state
-    for var in st.session_state.scores[1:]:
-        del var
-
-st.write(st.session_state.scores)
-st.write(st.session_state.scores[0])
-st.write(st.session_state.scores[0]["name"])
-
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
 
 
 
