4fb20af131d4bee001676ae1113cd427f08d2ce0
Murphys Law <65669647+MurphysLa-w@users.noreply.github.com>
2025-05-14 20:23:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,22 @@ import streamlit as st
 import pandas as pd
 
 
+st.text_input("Formula:", r"\frac{m_\txt{Wasser}}{V_\txt{Wasser}")
+
+
 st.write("# Variablen")
 df = pd.DataFrame(
     [
-       {"Formelzeichen": "m_\txt{Wasser}", "Einheit": "g", "Messwert": 16.8, "Fehler": 0.01, "Ist Konstant": True},
+       {"Formelzeichen": r"\rho_\txt{Wasser}", "Einheit": "g", "Messwert": 16.8, "Fehler": 0.01, "Ist Konstant": True},
+   ]
+)
+edited_df = st.data_editor(df)
+
+st.write("# Variablen")
+df = pd.DataFrame(
+    [
+       {"Formelzeichen": r"m_\txt{Wasser}", "Einheit": "g", "Messwert": 16.8, "Fehler": 0.01, "Ist Konstant": True},
    ]
 )
 edited_df = st.data_editor(df, num_rows="dynamic")
 
-st.write(edited_df.iat[1, 1])
-st.write(edited_df.iat[0, 4])
