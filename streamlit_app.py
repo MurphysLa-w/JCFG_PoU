2b2ea881ba327caa1893b6d6a8a8dee18e6cf7e3
Murphys Law <65669647+MurphysLa-w@users.noreply.github.com>
2025-05-14 23:10:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,4 +65,4 @@ if modeS:
 			PoU_SingleDeriv = PoU_SingleDeriv.replace(nAdd+chr(nameChr+106), orgName)
 		PoU_SingleDeriv = r"\begin{equation}\frac{\partial " + res_name + r"}{\partial " + name + "} = " + PoU_SingleDeriv + r"\end{equation}" # Modify for document
 		#st.write(PoU_SingleDeriv)
-		st.text(PoU_SingleDeriv)
+		st.latex(PoU_SingleDeriv)
