29b7c8dd8ca05ba5ee43a38a3c7e0cc07d60eb82
Murphys Law <65669647+MurphysLa-w@users.noreply.github.com>
2025-05-14 18:19:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,6 @@ with st.form("new_score", clear_on_submit=True):
     pushups = st.number_input("Pushups", key="pushups", step=1, value=0, min_value=0)
     situps = st.number_input("Situps", key="situps", step=1, value=0, min_value=0)
     st.form_submit_button("Submit", on_click=new_scores)
+
+if(st.button("Delete")):
+    del st.session_state[1]
