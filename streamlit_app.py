cfc95dd37aa7f005539762e34c13f8f4e0136942
Murphys Law <65669647+MurphysLa-w@users.noreply.github.com>
2025-05-15 03:27:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="JCFG",)
 st.title("Fehlerfortpflanzung nach Gauß")
 st.text("DISCLAIMER: Bullshit In, Bullshit Out")
 
-ACTIVE = st.text_input("Token:", placeholder="...") == st.secrets.global
+ACTIVE = st.text_input("Token:", placeholder="...") == st.secrets["db_username"]
 
 
 st.subheader("Errechnete Größe")
