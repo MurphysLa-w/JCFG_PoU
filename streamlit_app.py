89efc01061f2160517f7d7b6f604972f57ab98b8
Murphys Law <65669647+MurphysLa-w@users.noreply.github.com>
2025-05-17 14:32:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ for nameInd, name in enumerate(var_names):
 		hasError = True
 	elif any(	(name in bLname) and (nameInd != bLindex)
 		for bLindex, bLname in enumerate(blackList)):
-		st.error("Die " + str(nameInd+1) + ". Variable in der Tabelle ist als Zeichenfolge nicht eindeutig genug, da sie im Namen anderer Variablen oder Steuerwörtern aus Latex wie 'frac' vorkommt. \n\n Verlängern Sie z.B. den Namen 'c' zu 'c_\text{a}'", icon="🚨")
+		st.error("Die " + str(nameInd+1) + ". Variable in der Tabelle ist als Zeichenfolge nicht eindeutig genug, da sie im Namen anderer Variablen oder Steuerwörtern aus Latex wie 'frac' vorkommt. \n\n Verlängern Sie z.B. den Namen 'c' zu " + r"c_\text{a}", icon="🚨")
 		hasError = True
 	elif name not in formula:
 		st.error("Die " + str(nameInd+1) + ". Variable in der Tabelle kommt in der Formel nicht vor!", icon="🚨")
