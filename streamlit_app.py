394be0beea4979ca62f54e2e18394db9579acb31
Murphys Law <65669647+MurphysLa-w@users.noreply.github.com>
2025-06-21 20:40:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -317,7 +317,7 @@ if modeC and not hasError:
 	st.subheader("Errechneter Fehler")
 	if DEBUG: st.info("Vor Aufbereitung:   " + str(PoU_Calc))
 	PoU_Calc = PoU_Calc[3:]
-	PoU_Calc = regex.sub(r"(?<!Delta)(?<!+) (?=roc[a-z])", r" \\cdot " , PoU_Calc)		#Add * beteen two vars
+	PoU_Calc = regex.sub(r"(?<!Delta)(?<!\+) (?=roc[a-z])", r" \\cdot " , PoU_Calc)		#Add * beteen two vars
 	for nameChr, name in enumerate(var_names):
 		PoU_Calc = PoU_Calc.replace(r"\Delta " + nAdd+chr(nameChr+97), " * (" + str(var_uncert[nameChr]) + ")" )
 		PoU_Calc = PoU_Calc.replace(nAdd+chr(nameChr+97), str(var_values[nameChr]))
