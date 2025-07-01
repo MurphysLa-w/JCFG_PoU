f15328dc964f0f51ae34b67a646ec1fbb7e0d467
Murphys Law <65669647+MurphysLa-w@users.noreply.github.com>
2025-07-01 02:44:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,7 @@ if "data" not in st.session_state:
 
 
 # Setting Up Buttons
-col1, col2 = st.columns([1,15])
+col1, col2 = st.columns([1,10])
 with col1:
 	undo = st.button("",icon=":material/undo:", help="Rückgängig")
 with col2:
