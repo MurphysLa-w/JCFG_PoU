f4b7e0c8acf9da5798f990cbe725fb322747d3f0
Murphys Law <65669647+MurphysLa-w@users.noreply.github.com>
2026-03-08 01:36:20 +0100

    Update 1.6.1
    Migrated ExitCodes to new file
    Changed ExitCode Rendering

--- jcfg/__init__.py
+++ jcfg/__init__.py
@@ -3,11 +3,13 @@
 from .core import Variable, PoUInput, PoUOutput, PoUEngine
 from .session_manager import HistoryManager
 from .telemetry import submit_bug_report, log
-from .utils import ExitCode, display_ExitCodes, to_float_safe, to_str_safe
+from .exit_codes import ExitCode, display_ExitCodes, render_ExitCode
+from .utils import to_float_safe, to_str_safe
 
 __all__ = [
 	"Variable", "PoUInput", "PoUOutput", "PoUEngine",
 	"HistoryManager",
 	"submit_bug_report", "log",
-	"ExitCode", "display_ExitCodes", "to_float_safe", "to_str_safe"
+	"ExitCode", "render_ExitCode", "display_ExitCodes",
+	"to_float_safe", "to_str_safe"
 	]
