e96dd0ebc2b1a57161b88203eb29b0be893b4cea
Murphys Law <65669647+MurphysLa-w@users.noreply.github.com>
2026-04-07 01:01:17 +0200

    Fix streamlit_app.py
    Minor fix of brackets

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ __version__ = "1.7.0-beta"
 
 import streamlit as st
 
-pg = st.navigation([st.Page("pages/jcfg_calculator.py", title="Rechner"), st.Page("pages/jcfg_tutorial.py", title="Tutorial", visibility="hidden")])
+pg = st.navigation([st.Page("pages/jcfg_calculator.py", title="Rechner"), st.Page("pages/jcfg_tutorial.py", title="Tutorial"), visibility="hidden"])
 pg.run()
